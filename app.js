b1d0a79ef44182b3c8d9d9d73c7ebbd6b4421597
yamashima daiki <st072303@m02.kyoto-kcg.ac.jp>
2025-01-22 22:45:53 +0900

    commit1.1.1

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname)));
 app.use('/views', express.static(path.join(__dirname, 'views')));
 
 // データベース接続
